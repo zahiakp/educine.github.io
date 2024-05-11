a8f3fa9475077d5445714398b8a525de151ac575
zahiakp <mohammedzahiayikkarappadi@gmail.com>
2024-05-11 15:10:56 +0530

    hj

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2024, 5, 3, 15)}); // year, month, date, hour
+        });
+});		
+
